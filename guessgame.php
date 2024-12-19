3a7b00617ed96f6029469e2f4ab0e9d2ecad85e0
Manokha Vadim <manokha.vadim@gmail.com>
2024-12-19 18:59:39 +0200

    commit message

---
+++ guessgame.php
@@ -0,0 +1,47 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Праграмма угадай число</title>
+</head>
+
+<body>
+<?php
+if (isset($_POST['posted']))
+{
+	$number = rand(1,2);
+	if ($_POST['guess'] > $number)
+
+	{
+			echo "<h3>Ваше число слишком большое</h3>";
+			echo "<br>Загаданное число:$number, Вы проиграли, попробуйте еще раз </hr>";
+	}
+	else if ($_POST['guess'] < $number)
+	{
+			echo "<h3>Ваше число слишком маленькое</h3>";
+			echo "<br>Загаданное число:$number, Вы проиграли, попробуйте еще раз </hr>";
+	}
+	else if ($_POST['guess'] > $number)
+	{
+			echo "<h3>Ваше число слишком большое</h3>";
+			echo "<br>Загаданное число:$number, Вы проиграли, попробуйте еще раз </hr>";
+	}
+	else
+	{
+			echo "<br>Загаданное число:
+			 $number, Вы выиграли? </hr>";
+	}
+}		
+?>
+
+<form method="post" action="guessgame.php">
+<input type="hidden" name="posted" value="true">
+Угадай число от 1 до 2
+<input type="guess" type="text">
+<br>
+<br>
+<input type="submit" value="Отправить">
+</form>
+
+</body>
+</html>
---
+++ if.php
@@ -0,0 +1,38 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Документ без названия</title>
+</head>
+<?php
+
+$a = 1;
+$b = 1;
+$w = 6;
+$f = 6;
+	$dor = "fas";
+	$faq = "fas";
+
+
+if ($a == $b && $w == $f)
+{
+	echo "Преременные \"равны\"";
+	
+
+	if ($dor == $faq)
+	{
+	echo "<br>В переменных разные имена	";
+	}
+	
+}
+
+else  
+{
+	echo "Преременные \"неравны\"";
+	
+}
+
+?>
+<body>
+</body>
+</html>
